791f8acd4afb04e85c94b0ff7b353e586a6ae948
v3nturetheworld <pheinenjr@gmail.com>
2015-05-13 23:03:49 -0600

    Started MCTS

--- c++/MCTS.cpp
+++ c++/MCTS.cpp
@@ -1,32 +1,119 @@
 #include <vector>
+#include "MCTS.h"
 
 MCTS::MCTS(){}
 
 //Are data type to be put in the tree
-struct MCNode(int children, Move m, uint32_t location, double payOff, int samples)
+struct MCNode
 {
-   int myChildren = children;
-   Move myMove = m;
-   uint32_t myLocation = location;
-   double myPayoff = payOff;
-   int totalSamples = samples;
+   int myChildren = 0;
+   Move myMove = {0,0};
+   uint32_t location = 0;
+   double myPayoff = 0.0;
+   int totalSamples = 0;
+   //Constructor
+   MCNode(){};
+   MCNode(int children, Move m, uint32_t location, double payOff, int samples)
+   {
+	  this->children = children;
+	  this->m = m;
+	  this->location = location;
+	  this->payOff = payOff;
+	  this->samples = samples;
+   }
+   int getChildren() return children;
+   Move getMove() return m;
+   uint32_t getLocation() return location;
+   double getPayOff() return payOff;
+   int getSamples() return samples;
 };
 
-std::vector<Move> tree;
-//Figure this bit out later
-ChineseCheckersState &state;
-std::vector<Move> moves;
-state.getMoves(moves);
+//Initializes the tree
+std::vector<MCNode> MCTS::InitializeTree(ChineseCheckersState &state)
+{
+   //Initialize stuff
+   std::vector<Move> moves;
+   state.getMoves(moves);
+   std::vector<MCNode> tree;
+   //Initialize the root of the tree to the first Node,
+   //and num challenge 
+   MCNode rootNode(moves.size(), moves.at(0), 0, 0.0, 0);
+   tree.push_back(); //Add to tree
+
+}
+////////////////////////
+/////Some UCB1 Code/////
+////////////////////////
+unsigned getRand()
+{
+   std::random_device rd;
+   std::mt19937 gen(rd());
+   std::uniform_real_distribution<> dis(0, 1);
+   double rand = (double)dis(gen);
+   std::cerr << "rand = " << rand << std::endl;
+   return rand;
+}
+double MCTS::random(Move aMove, ChineseCheckersState &s){
+   //Call a state
+   //
+   //NOTE, WE NEED a new CCState, or else more work will be needed
+   ChineseCheckersState state(s);
+   int cPlayer = state.getCurrentPlayer();
+   //std::cerr << "State Cloned! Applying move, " << aMove << " checking current player in cloned: " << cPlayer << std::endl;
+   //std::cerr << "CURRENT PLAYER CHECK original s: " << s.getCurrentPlayer() << std::endl;
+   state.applyMove(aMove);
+   //std::cerr << "Player after apply move check: " << cPlayer << std::endl;
+   std::vector<Move> moves;
+   state.getMoves(moves);
+   //std::cerr << "Player after get move check: " << cPlayer << std::endl;
+  //Increase our totalSamples, as another game is being sampled
+  auto rand = getRand();
+  totalSample++;
+  //std::cerr << "sample increased, now = " << totalSample << std::endl;
+
+ 
+  //Playout Policy 2
+  //This policy return the results being evaluated by the eval function
+    std::cerr << "Policy 2 being used " << std::endl;
+   //Allow to play to x moves
+   const int movesToPlay = 75;
+   for (int i = 0; i < movesToPlay; ++i){
+	  //Check if the game is over, if so run eval function
+	  if(state.gameOver()) return (double) state.eval();
+
+	  state.getMoves(moves); //We need to get the moves each time this time
+	  if(rand > 0.15){
+	  //Note, Again, this Might be wrong!!!
+	  std::sort(moves.begin(), moves.end(), [](const Move &a, const Move &b){
+		 std::cerr << "lambda compare return" << a.score < b.score << std::endl; 
+		 return a.score < b.score;});
+	  Move bestMove = moves.at(0);	 
+	  std::cerr << "Best move from Lambda Policy 1 = " << bestMove << std::endl;
+			//Apply Best Move
+		 state.applyMove(bestMove);
+			
+	  } else {
+		 //only happens 15% of the time
+		 state.applyMove(moves.at((double)(getRand() * moves.size())));
+	  }
+   }
+   //Return the eval function
+   return (double) state.eval();
+ 
+}
+
+
+
 // the high-level function for computing the best move
 Move MCTS::GetBestMove()
 {
 
+   return bestMove;
 }
 // traverse down the tree (recursively),
 // returning the value at the leaf of the sample
 double MCTS::SelectLeaf(uint32_t node)
 {
-
    return 0.0;
 }
 // Use the UCB rule to find the best child
--- c++/MCTS.h
+++ c++/MCTS.h
@@ -2,9 +2,14 @@
 #include "ChineseCheckersState.h"
 
 public:
+//Initialize the Tree
+std::vector<Move> InitializeTree(ChineseCheckersState &state);
+//Running total
+int total = 0;
 // the high-level function for computing the best move
 Move GetBestMove();
-// traverse down the tree (recursively), returning the value at the leaf of the sample
+// traverse down the tree (recursively),
+// returning the value at the leaf of the sample
 double SelectLeaf(uint32_t node); 
 // Use the UCB rule to find the best child
 uint32_t SelectBestChild(uint32_t node); 
@@ -16,6 +21,8 @@ bool IsLeaf(uint32_t node);
 void Expand(uint32_t node);
 // play out the game, returning the evaluation at the end of the game
 double DoPlayout(uint32_t node);
+
+double random(Move aMove, ChineseCheckersState &s);
 
 private:
  
